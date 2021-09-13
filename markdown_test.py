971770b66dc1e1a7a472235f9482c117461f3837
jmasonlee <jmasonlee@gmail.com>
2021-09-13 07:37:26 -0600

    Initial state

---
+++ markdown_test.py
@@ -0,0 +1,117 @@
+import unittest
+
+from markdown import (
+    parse,
+)
+
+from approvaltests.approvals import verify_all
+
+
+# Tests adapted from `problem-specifications//canonical-data.json`
+
+
+def test_all_combinations():
+    input_strings = ["""# Header 1
+## Header 2
+###### Header 6
+*  A thing 
+* __ A bold thing in a list __
+* _An italic thing in a list_ 
+
+* __A bold thing __
+
+* _An italic thing_
+
+ __A bold thing __
+
+ _An italic thing_
+
+<h4> hello </h4>
+<ul>
+<li> One </li>
+</ul>
+<p> Hello! </p>
+"""]
+    results = []
+    for input_string in input_strings:
+        results.append(parse(input_string))
+    verify_all("markdown", results)
+
+
+class MarkdownTest(unittest.TestCase):
+    def test_parses_normal_text_as_a_paragraph(self):
+        self.assertEqual(
+            parse("This will be a paragraph"), "<p>This will be a paragraph</p>"
+        )
+
+    def test_parsing_italics(self):
+        self.assertEqual(
+            parse("_This will be italic_"), "<p><em>This will be italic</em></p>"
+        )
+
+    def test_parsing_bold_text(self):
+        self.assertEqual(
+            parse("__This will be bold__"), "<p><strong>This will be bold</strong></p>"
+        )
+
+    def test_mixed_normal_italics_and_bold_text(self):
+        self.assertEqual(
+            parse("This will _be_ __mixed__"),
+            "<p>This will <em>be</em> <strong>mixed</strong></p>",
+        )
+
+    def test_with_h1_header_level(self):
+        self.assertEqual(parse("# This will be an h1"), "<h1>This will be an h1</h1>")
+
+    def test_with_h2_header_level(self):
+        self.assertEqual(parse("## This will be an h2"), "<h2>This will be an h2</h2>")
+
+    def test_with_h6_header_level(self):
+        self.assertEqual(
+            parse("###### This will be an h6"), "<h6>This will be an h6</h6>"
+        )
+
+    def test_unordered_lists(self):
+        self.assertEqual(
+            parse("* Item 1\n* Item 2"), "<ul><li>Item 1</li><li>Item 2</li></ul>"
+        )
+
+    def test_with_a_little_bit_of_everything(self):
+        self.assertEqual(
+            parse("# Header!\n* __Bold Item__\n* _Italic Item_"),
+            "<h1>Header!</h1><ul><li><strong>Bold Item</strong></li><li><em>Italic Item</em></li></ul>",
+        )
+
+    def test_with_markdown_symbols_in_the_header_text_that_should_not_be_interpreted(
+            self,
+    ):
+        self.assertEqual(
+            parse("# This is a header with # and * in the text"),
+            "<h1>This is a header with # and * in the text</h1>",
+        )
+
+    def test_with_markdown_symbols_in_the_list_item_text_that_should_not_be_interpreted(
+            self,
+    ):
+        self.assertEqual(
+            parse("* Item 1 with a # in the text\n* Item 2 with * in the text"),
+            "<ul><li>Item 1 with a # in the text</li><li>Item 2 with * in the text</li></ul>",
+        )
+
+    def test_with_markdown_symbols_in_the_paragraph_text_that_should_not_be_interpreted(
+            self,
+    ):
+        self.assertEqual(
+            parse("This is a paragraph with # and * in the text"),
+            "<p>This is a paragraph with # and * in the text</p>",
+        )
+
+    def test_unordered_lists_close_properly_with_preceding_and_following_lines(self):
+        self.assertEqual(
+            parse("# Start a list\n* Item 1\n* Item 2\nEnd a list"),
+            "<h1>Start a list</h1><ul><li>Item 1</li><li>Item 2</li></ul><p>End a list</p>",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
