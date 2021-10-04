fed2de0ccb54359602915c868158e87bb8587170
jmasonlee <jmasonlee@gmail.com>
2021-10-04 11:15:05 -0600

    r extract method

--- markdown.py
+++ markdown.py
@@ -9,24 +9,16 @@ def parse(markdown):
     in_list_append = False
     for line in lines:
         if re.match('###### (.*)', line):
-            tag = 'h6'
-            string = line[7:]
-            line = wrap_string_in_tag(string, tag)
+            line = wrap_string_in_tag(line[7:], 'h6')
         elif re.match('## (.*)', line):
-            tag = 'h2'
-            string = line[3:]
-            line = wrap_string_in_tag(string, tag)
+            line = wrap_string_in_tag(line[3:], 'h2')
         elif re.match('# (.*)', line):
-            string = line[2:]
-            tag = 'h1'
-            line = wrap_string_in_tag(string, tag)
+            line = wrap_string_in_tag(line[2:], 'h1')
 
         line_starts_with_asterisk_regex_match = re.match(r'\* (.*)', line)
         if line_starts_with_asterisk_regex_match and in_list:
             in_list = True
-            string = italicize(line_starts_with_asterisk_regex_match.group(1))
-            tag = 'li'
-            line = wrap_string_in_tag(string, tag)
+            line = wrap_string_in_tag(italicize(line_starts_with_asterisk_regex_match.group(1)), 'li')
             item = line
             new_i = item
         elif in_list:
@@ -35,18 +27,14 @@ def parse(markdown):
             new_i = line
         elif line_starts_with_asterisk_regex_match:
             in_list = True
-            string = line_starts_with_asterisk_regex_match.group(1)
-            tag = 'li'
-            line = wrap_string_in_tag(string, tag)
+            line = wrap_string_in_tag(line_starts_with_asterisk_regex_match.group(1), 'li')
             list_item = line
             new_i = '<ul>' + list_item
         else:
             new_i = line
         m = re.match('<h|<ul|<p|<li', new_i)
         if not m:
-            string = new_i
-            tag = 'p'
-            line = wrap_string_in_tag(string, tag)
+            line = wrap_string_in_tag(new_i, 'p')
             new_i = line
 
         new_i = add_emphasis(new_i)
