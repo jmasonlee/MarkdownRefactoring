96a8f82e130dd1f9cba372a2dbe3e3f509aec63d
jmasonlee <jmasonlee@gmail.com>
2021-10-04 13:09:43 -0600

    R new_line uses list

--- markdown.py
+++ markdown.py
@@ -24,18 +24,18 @@ def parse(markdown):
             if in_list:
                 line = wrap_string_in_tag(italicize(line_starts_with_asterisk_regex_match.group(1)), 'li')
                 list += line
-                new_line = line
+                new_line = list
                 break
             else:
                 in_list = True
                 line = wrap_string_in_tag(line_starts_with_asterisk_regex_match.group(1), 'li')
                 list_item = line
-                list += line
+                list += '<ul>' + list_item
                 new_line = '<ul>' + list_item
                 break
             # Otherwise add an italicized list item
             # Grab the next line
-            line_starts_with_asterisk_regex_match = re.match(r'\* (.*)', line)
+            line_starts_with_asterisk_regex_match = re.match(r'\* (.*)', lines[i])
 
         #wrap list in ul
         if line_starts_with_asterisk_regex_match and in_list:
