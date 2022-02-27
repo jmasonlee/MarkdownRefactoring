3439896e888102fda70e3c858570c1375bebfccd
jmasonlee <jmasonlee@gmail.com>
2022-02-26 19:32:23 -0700

    R replace truthy with bool

--- markdown.py
+++ markdown.py
@@ -37,7 +37,7 @@ def handle_list(in_list, new_line, post_process):
     line_starts_with_asterisk_regex_match = re.match(r'\* (.*)', new_line)
     is_list_item = bool(line_starts_with_asterisk_regex_match)
     new_line = start_list(in_list, new_line) if is_list_item else new_line
-    if line_starts_with_asterisk_regex_match:
+    if is_list_item:
         list_item = line_starts_with_asterisk_regex_match.group(1)
         new_line = start_list(in_list, new_line)
         if in_list:
